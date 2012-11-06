737fb16ba0417cbb577f85c19951b8c66f21dcde
Nikhil Sama <nikhilsama@gmail.com>
2012-11-06 19:45:29 +0530

    MixPanel

---
+++ app/Plugin/Mixpanel/Controller/Component/MixpanelComponent.php
@@ -0,0 +1,67 @@
+<?php
+class MixpanelComponent extends Component {
+	public $components = array(
+		'Session',
+	);
+	
+	public function initialize(Controller $controller) {
+		$this->settings = array(
+			'token' => Configure::read('Mixpanel.token'),
+			'properties' => array(),
+		);
+		if (!$this->Session->check('Mixpanel.events')) {
+			$this->Session->write('Mixpanel.events', array());
+		}
+	}
+	
+	public function beforeRender(Controller $controller) {
+		Configure::write('Mixpanel.events', $this->Session->read('Mixpanel.events'));
+		Configure::write('Mixpanel.register', $this->Session->read('Mixpanel.register'));
+		Configure::write('Mixpanel.settings', $this->settings);
+		$this->Session->delete('Mixpanel.events');
+		$this->Session->delete('Mixpanel.register');
+	}
+	
+	public function name_tag($name) {
+		$this->settings['name_tag'] = $name;
+	}
+	
+	public function identify($id) {
+		$this->settings['identify'] = $id;
+	}
+	
+	public function people($id, $properties = array()) {
+		$this->settings['people']['identify'] = $id;
+		$this->settings['people']['set'] = $properties;
+	}
+
+/**
+ * Register new properties using mixpanel.register(), accepts a key => value array of properties
+ * Sending a key => value with a duplicate key replaces the old value
+ *
+ * @param array $properties Array of key => value properties to register
+ * @return void
+ * @author David Kullmann
+ */
+	public function register($properties) {
+		$register = $this->Session->read('Mixpanel.register');
+		if (!empty($properties)) {
+			foreach($properties as $key => $value) {
+				$register[$key] = $value;
+			}	
+		}
+		$this->Session->write('Mixpanel.register', $register);
+	}
+	
+	public function track($event, $properties = array()) {
+		$events = $this->Session->read('Mixpanel.events');
+		$events[] = compact('event', 'properties');
+		$this->Session->write('Mixpanel.events', $events);
+	}
+	
+	public function trackInternal($event, $properties = array()) {
+		$this->track($event, $properties);
+		// TODO replace this with an option to send the event straight to mixpanel
+		// for potential implementation see http://petewarden.typepad.com/searchbrowser/2008/06/how-to-post-an.html
+	}
+}
---
+++ app/Plugin/Mixpanel/View/Helper/MixpanelHelper.php
@@ -0,0 +1,55 @@
+<?php
+App::uses('AppHelper', 'View/Helper');
+class MixpanelHelper extends AppHelper {
+	public $name = 'Mixpanel';
+	
+	function test() {
+		// debug($this->_View->request);
+		return '';
+	}
+	
+	function embed() {
+		$include = <<<HTML
+<!-- start Mixpanel --><script type="text/javascript">(function(c,a){window.mixpanel=a;var b,d,h,e;b=c.createElement("script");b.type="text/javascript";b.async=!0;b.src=("https:"===c.location.protocol?"https:":"http:")+'//cdn.mxpnl.com/libs/mixpanel-2.1.min.js';d=c.getElementsByTagName("script")[0];d.parentNode.insertBefore(b,d);a._i=[];a.init=function(b,c,f){function d(a,b){var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(Array.prototype.slice.call(arguments,0)))}}var g=a;"undefined"!==typeof f?
+g=a[f]=[]:f="mixpanel";g.people=g.people||[];h="disable track track_pageview track_links track_forms register register_once unregister identify name_tag set_config people.identify people.set people.increment".split(" ");for(e=0;e<h.length;e++)d(g,h[e]);a._i.push([b,c,f])};a.__SV=1.1})(document,window.mixpanel||[]);
+mixpanel.init(TOKEN);
+TRACKERS</script><!-- end Mixpanel -->
+HTML;
+
+		$settings = Configure::read('Mixpanel.settings');
+		$events   = Configure::read('Mixpanel.events');
+		$register = Configure::read('Mixpanel.register');
+		
+		$trackers = array();
+		# Integration
+		if (Configure::read('debug')) $trackers[] = 'mixpanel.set_config({debug: true});';
+		if (isset($settings['identify'])) $trackers[] = sprintf('mixpanel.identify(%s);', json_encode($settings['identify']));
+		if (isset($settings['name_tag'])) $trackers[] = sprintf('mixpanel.name_tag(%s);', json_encode($settings['name_tag']));
+		if (isset($register)) $trackers[] = sprintf('mixpanel.register(%s);', json_encode($register));
+
+		if (!empty($events)) {
+			foreach ($events as $event) {
+				$properties = $event['properties'];
+				$properties = array_merge($settings['properties'], $properties);
+
+				$trackers[] = sprintf(
+					'mixpanel.track(%s, %s);',
+					json_encode($event['event']),
+					(!empty($properties)) ? json_encode($properties) : '{}'
+				);
+			}
+		}
+
+		#People
+		if (isset($settings['people'])) {
+			 $trackers[] = sprintf('mixpanel.people.identify(%s);', json_encode($settings['people']['identify']));
+			 $trackers[] = sprintf('mixpanel.people.set(%s);', json_encode($settings['people']['set']));
+		}
+		
+		return str_replace(
+			array('TOKEN', 'TRACKERS'),
+			array(json_encode($settings['token']), join("\n", $trackers)),
+			$include
+		);
+	}
+}
