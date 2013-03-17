cf2d8ccad56667aad1ba43ce0c119dcd140dcb9b
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-03-17 12:18:22 +0530

    Added Minify in these controllers

--- app/Controller/OffersController.php
+++ app/Controller/OffersController.php
@@ -6,7 +6,7 @@ App::uses('AppController', 'Controller');
  * @property Offer $Offer
  */
 class OffersController extends AppController {
-
+	public $helpers = array('Minify.Minify');
 /**
  * index method
  *
--- app/Controller/UserAddressesController.php
+++ app/Controller/UserAddressesController.php
@@ -6,7 +6,7 @@ App::uses('AppController', 'Controller');
  * @property UserAddress $UserAddress
  */
 class UserAddressesController extends AppController {
-
+	public $helpers = array('Minify.Minify');
 /**
  * index method
  *
