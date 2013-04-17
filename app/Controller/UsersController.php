2430a6ebf809cd81644adc7f1165dd8d951f6a73
swapnil <nswapt@gmail.com>
2013-04-17 23:04:22 +0530

    carousel image uploading option in vendor controller

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -153,12 +153,12 @@ class UsersController extends AppController {
             $message = 'The fun and easy way to give <b><u>free</u></b> gift vouchers to facebook friends';
             $this->Product->unbindModel(array('hasMany' => array('Gift','UploadedProductCode'),
                                                                            'belongsTo' => array('Vendor','ProductType','GenderSegment','AgeSegment','CodeType','Gift')));
-            $Image = $this->Product->find('all',array('fields' =>'DISTINCT Product.vendor_id' ,'conditions' => array('Product.display_order >'=>0)));
+            $Image = $this->Product->find('all',array('fields' =>'DISTINCT Product.vendor_id' ,'conditions' => array('Product.display_order >'=>0),'limit'=>10,'order' => array('RAND()')));
             $Image_new = array();
             foreach ($Image as $Images) {
                 $id=$Images['Product']['vendor_id'];
                 $this->Vendor->unbindModel(array('hasMany' => array('Product')));
-                $Image_new[] = $this->Vendor->find('all',array('fields' =>array('Vendor.wide_image'),'conditions' => array('Vendor.id '=>$id)));
+                $Image_new[] = $this->Vendor->find('all',array('fields' =>array('Vendor.carousel_image'),'conditions' => array('Vendor.id '=>$id)));
                 $this->set('Images', $Image_new);
             }
         
