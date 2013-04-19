164138d375374331e07ad40317e2e7f53f1094ad
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-04-20 00:33:24 +0530

    reduce carousel image up to 5 only

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -159,7 +159,7 @@ class UsersController extends AppController {
 
             $this->Product->unbindModel(array('hasMany' => array('Gift','UploadedProductCode'),
                                                                            'belongsTo' => array('Vendor','ProductType','GenderSegment','AgeSegment','CodeType','Gift')));
-            $Image = $this->Product->find('all',array('fields' =>'DISTINCT Product.vendor_id' ,'conditions' => array('Product.display_order >'=>0),'limit'=>10,'order' => array('RAND()')));
+            $Image = $this->Product->find('all',array('fields' =>'DISTINCT Product.vendor_id' ,'conditions' => array('Product.display_order >'=>0),'limit'=>5,'order' => array('RAND()')));
             $Image_new = array();
             foreach ($Image as $Images) {
                 $id=$Images['Product']['vendor_id'];
