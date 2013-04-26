f23179fd297a7aef8ea0058e20b2203c5c00bd4b
Shubham <shubham150@gmail.com>
2013-04-26 13:01:18 +0530

    changes in user controller

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -194,13 +194,10 @@ class UsersController extends AppController {
                 if($gift){
                     $sender_name = $gift['Sender']['UserProfile']['first_name'];
                     $this->Reminder->recursive = -1;
-                    $receiver_name = $this->Reminder->find('first', array('fields' => array('friend_name','friend_birthday'),'conditions' => array('friend_fb_id' => $gift['GiftsReceived']['receiver_fb_id'])));
+                    $receiver_name = $this->Reminder->find('first', array('fields' => array('friend_name'),'conditions' => array('friend_fb_id' => $gift['GiftsReceived']['receiver_fb_id'])));
                     if($receiver_name['Reminder']['friend_name'])
                         $receiver_name = $receiver_name['Reminder']['friend_name'];
-                        $receiver_name = explode(" ", $receiver_name);
                     else $receiver_name = 'you';
-                    if($receiver_name['Reminder']['friend_birthday'])
-                        $receiver_birthday = $receiver_name['Reminder']['friend_birthday'];
                     $vendor_name = $gift['Product']['Vendor']['name'];
                     $image = $gift['Product']['Vendor']['facebook_image'];
                     $value = $gift['GiftsReceived']['gift_amount'];
@@ -223,11 +220,8 @@ class UsersController extends AppController {
             
             $this->set('slidePlaySpeed', $slidePlaySpeed);
             $this->set('fb_url', FULL_BASE_URL.$_SERVER[ 'REQUEST_URI' ]);
-            if (isset($vendor_name) && $receiver_birthday == date('Y-m-d')) {
-                $this->set('fb_title', $receiver_name[0].", Here's a gift to celebrate your wisest year yet. Happy birthday!");
-            }
             if (isset($vendor_name)) {
-                $this->set('fb_title', $receiver_name[0].", I hope this gift makes you feel as great as you are.");
+                $this->set('fb_title', " Surprise! ". $sender_name." has sent a gift to ".$receiver_name.". Visit Giftology to unwrap the gift.");
             } else {
                 $this->set('fb_title', "Giftology | Don't just post on Facebook make it a gift post! ");
             }
