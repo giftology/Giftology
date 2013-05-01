ffef126dddce7955a1f0f4e3122356b2ba4d24e5
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-05-01 15:16:30 +0530

    no fb_id_array if fql doesn not return data

--- app/Controller/RemindersController.php
+++ app/Controller/RemindersController.php
@@ -343,14 +343,17 @@ class RemindersController extends AppController {
 		$friends= array();
         $friends = $Facebook->api(array('method' => 'fql.query',
                                         'query' => 'SELECT uid FROM user WHERE uid IN (SELECT uid2 from friend where uid1=me()) order by rand() limit 50'));
-         $fb_id_array =array();
-         foreach ($friends as $frnd){
-         $fb_id_array[]= $frnd['uid'];    
-         }
-   		 $this->set('facebook_id',$fb_id_array);
+        $fb_id_array =array();
+        if(isset($friends) && !empty($friends)){
+        	foreach ($friends as $frnd){
+        		$fb_id_array[]= $frnd['uid'];    
+        	}
+   		 	$this->set('facebook_id',$fb_id_array);	
+        }
+        
 		$users = $this->UserProfile->find('first', array('fields' => array('id','user_id'), 'conditions' => array('user_id' => $this->Auth->user('id'))));
 		$fb_id = $this->User->find('first',array('fields' => array('id','facebook_id'),'conditions' => array('User.id' => $users['UserProfile']['user_id'])));
-		$Facebook = new FB();
+		
 		$fb_first_last_name = $Facebook->api(array('method' => 'fql.query',
                                     'query' => 'SELECT first_name, last_name FROM user WHERE uid = '.$fb_id['User']['facebook_id']));
 		$data = array(
