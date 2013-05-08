0e13c83cc7e736fb4cb25f0ae9d5e54a3bde9c20
Shubham <shubham150@gmail.com>
2013-05-08 16:47:21 +0530

    products

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -175,7 +175,7 @@ class UsersController extends AppController {
             }
              $this->Product->unbindModel(array('hasMany' => array('Gift','UploadedProductCode'),
                                                                            'belongsTo' => array('ProductType','GenderSegment','AgeSegment','CodeType','Gift')));
-            $product = $this->Product->find('all',array('conditions' => array('Product.display_order >'=>0),'limit'=>5));
+            $product = $this->Product->find('all',array('conditions' => array('Product.display_order >'=>0),'limit'=>6));
             $this->set('products', $product);
 
             $slidePlaySpeed = 8000;
