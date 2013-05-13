a19b7fcc536e53aa15d4919372a91fc7d7e59c8c
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-05-13 20:18:15 +0530

    logging ws send URL

--- app/Controller/GiftsController.php
+++ app/Controller/GiftsController.php
@@ -88,7 +88,7 @@ class GiftsController extends AppController {
 
 	public function ws_send () {
         $url = array($this->params->query, $this->params->url);
-        $this->log("Logging ws_send url ".serialize($urls));
+        $this->log("Logging ws_send url ".serialize($url));
 		$sender_id = isset($this->params->query['sender_id']) ? $this->params->query['sender_id'] : null;
 		$receiver_fb_id = isset($this->params->query['receiver_fb_id']) ? $this->params->query['receiver_fb_id'] : null;
 		$product_id = isset($this->params->query['product_id']) ? $this->params->query['product_id'] : null;
