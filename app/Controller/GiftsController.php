915759cffe599574b5202e6a05be0ee63430efc1
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-05-14 10:23:09 +0530

    ws_list serialization log message correction

--- app/Controller/GiftsController.php
+++ app/Controller/GiftsController.php
@@ -36,7 +36,7 @@ class GiftsController extends AppController {
 		$this->log("Dont for recv fb id ".$receiver_fb_id);
 		$e = $this->wsListGiftException($receiver_fb_id );
 		if(isset($e) && !empty($e)){
-            $this->log("Logging exception for ".$receiver_fb_id." ".serialize($e));
+            $this->log("Logging gift received exception for ".$receiver_fb_id." ".serialize($e));
             $this->set('gifts', array('error' => $e));
         }
 		else{
