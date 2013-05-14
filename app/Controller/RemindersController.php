a83b780bdba8c035dc02edeee9c314c49e31ce10
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-05-14 19:32:37 +0530

    ws_reminder_today
    web services for today birthday reminders

--- app/Controller/RemindersController.php
+++ app/Controller/RemindersController.php
@@ -28,6 +28,28 @@ class RemindersController extends AppController {
 	    return parent::isAuthorized($user);
 	}
 
+	public function ws_reminder_today(){
+		DebugBreak();
+        $user_id = isset($this->params->query['user_id']) ? $this->params->query['user_id'] : null;
+        $conditions = array(
+            'user_id' => $user_id);
+
+        $conditions['MONTH(friend_birthday)'] = date('m');
+        $conditions['DAY(friend_birthday)'] = date('d'); 
+		$e = $this->wsReminderTodayException($user_id, $conditions);
+		if(isset($e) && !empty($e)) $this->set('today_birthday', array('error' => $e));
+        else{
+            $this->log("Searching today birthday reminder for ".$user_id);
+            $reminders = $this->Reminder->find('all',
+                array('conditions' => $conditions,
+                    'order' => 'friend_birthday ASC'
+            ));
+            //DebugBreak();
+            $this->set('today_birthday', $reminders);    
+        }
+		$this->set('_serialize', array('today_birthday'));
+	}
+
 
 /**
  * index method
@@ -874,5 +896,17 @@ class RemindersController extends AppController {
 
            //   $this->set('login_needed',"Sorry please do login to verify USer id.");
      // }
+    }
+
+    public function wsReminderTodayException($user_id, $conditions){
+    	$e = array();
+    	$reminders_count = $this->Reminder->find('count',
+                array('conditions' => $conditions,
+                    'order' => 'friend_birthday ASC'
+            ));
+    	if(!$reminders_count){
+    		$e[1] = "No birthday reminder today";
+    	}
+    	return $e;
     }
 }
