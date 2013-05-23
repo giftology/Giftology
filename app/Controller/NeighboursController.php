20d7a3b7d50fcd5b4049971f3ab60ea7be681361
Shubham <shubham150@gmail.com>
2013-05-23 11:37:29 +0530

    city segments changes

---
+++ app/Controller/NeighboursController.php
@@ -0,0 +1,17 @@
+<?php
+App::uses('AppController', 'Controller');
+App::uses('CakeEmail', 'Network/Email');
+
+/**
+ * Products Controller
+ *
+ * @property Product $Product
+ */
+class NeighbourCitiesController extends AppController {
+	public $helpers = array('Minify.Minify');   
+
+	public function index()
+	{
+		
+	}
+}
