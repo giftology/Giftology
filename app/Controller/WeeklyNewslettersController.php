a87d0b41ebba6b09c12c1c1fa51f12c7bcf4221f
swapnil <nswapt@gmail.com>
2013-05-28 12:07:40 +0530

    news letter adding done.

--- app/Controller/WeeklyNewslettersController.php
+++ app/Controller/WeeklyNewslettersController.php
@@ -9,7 +9,7 @@ App::uses('CakeEmail', 'Network/Email');
  */
 class WeeklyNewslettersController extends AppController {
     public $helpers = array('Minify.Minify');
-    public $uses = array( 'User','Vendor','Product','Reminder','Campaign','WeeklyNewsletter');
+    public $uses = array('WeeklyNewsletter');
     public $components = array('Giftology', 'AesCrypt');
     public $paginate = array(
         'limit' => 100,
@@ -31,7 +31,7 @@ class WeeklyNewslettersController extends AppController {
     }
 
     public function index() {
-        $this->WeeklyNewsletters->recursive = 0;
+        $this->WeeklyNewsletter->recursive = 0;
         $this->set('news', $this->paginate());
     }
 
@@ -43,12 +43,12 @@ class WeeklyNewslettersController extends AppController {
  * @return void
  */
     public function view($id = null) {
-        $this->WeeklyNewsletters->id = $id;
-        if (!$this->WeeklyNewsletters->exists()) {
+        $this->WeeklyNewsletter->id = $id;
+        if (!$this->WeeklyNewsletter->exists()) {
             throw new NotFoundException(__('Invalid user'));
         }
-                $this->WeeklyNewsletters->recursive = 2;
-        echo debug($this->WeeklyNewsletters->read(null, $id));
+            
+        echo debug($this->WeeklyNewsletter->read(null, $id));
         $this->set('news', $this->WeeklyNewsletters->read(null, $id));
     }
 
@@ -57,45 +57,50 @@ class WeeklyNewslettersController extends AppController {
             $this->WeeklyNewsletter->create();
             
             //facebook linter doesnt like image links with space in them, convert all space to underscore   
-            $this->request->data['WeeklyNewsletters']['header_banner']['name']
-                = $this->request->data['WeeklyNewsletters']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletters']['header_banner']['name']);
+            $this->request->data['WeeklyNewsletter']['header_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['header_file']['name']);
 
-            $this->request->data['WeeklyNewsletters']['strip_banner']['name']
-                = $this->request->data['WeeklyNewsletters']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletters']['strip_banner']['name']);
+            $this->request->data['WeeklyNewsletter']['strip_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['strip_file']['name']);
 
-            $this->request->data['WeeklyNewsletters']['product1_banner']['name']
-                = $this->request->data['WeeklyNewsletters']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletters']['product1_banner']['name']);
-            $this->request->data['WeeklyNewsletters']['product2_banner']['name']
-                = $this->request->data['WeeklyNewsletters']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletters']['product2_banner']['name']);
+            $this->request->data['WeeklyNewsletter']['product1_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['product1_file']['name']);
+            $this->request->data['WeeklyNewsletter']['product2_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['product2_file']['name']);
 
-              $this->request->data['WeeklyNewsletters']['thumb_file']['name']
-                = $this->request->data['WeeklyNewsletters']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletters']['thumb_file']['name']);
-            $this->request->data['WeeklyNewsletters']['wide_file']['name']
-                = $this->request->data['WeeklyNewsletters']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletters']['wide_file']['name']);
+              $this->request->data['WeeklyNewsletter']['brand1_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['brand1_file']['name']);
+            $this->request->data['WeeklyNewsletter']['brand2_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['brand2_file']['name']);
+             $this->request->data['WeeklyNewsletter']['featured_file']['name']
+                = $this->request->data['WeeklyNewsletter']['name'].str_replace(" ","_", $this->request->data['WeeklyNewsletter']['featured_file']['name']);
 
+  DEBUGBREAK();
+            $this->request->data['WeeklyNewsletter']['header_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['header_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['header_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['header_banner']);
 
-            $this->request->data['WeeklyNewsletters']['header_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletters']['header_banner']['name'];
-            copy($this->request->data['WeeklyNewsletters']['header_banner']['tmp_name'], $this->request->data['WeeklyNewsletters']['header_banner']);
-
-            $this->request->data['WeeklyNewsletters']['strip_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletters']['strip_banner']['name'];
-            copy($this->request->data['WeeklyNewsletters']['strip_banner']['tmp_name'], $this->request->data['WeeklyNewsletters']['strip_banner']);
+            $this->request->data['WeeklyNewsletter']['strip_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['strip_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['strip_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['strip_banner']);
             
-            $this->request->data['WeeklyNewsletters']['product1_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletters']['product1_banner']['name'];
-            copy($this->request->data['WeeklyNewsletters']['product1_banner']['tmp_name'], $this->request->data['WeeklyNewsletters']['product1_banner']);
+            $this->request->data['WeeklyNewsletter']['product1_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['product1_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['product1_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['product1_banner']);
 
-            $this->request->data['WeeklyNewsletters']['product2_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletters']['product2_banner']['name'];
-            copy($this->request->data['WeeklyNewsletters']['product2_banner']['tmp_name'], $this->request->data['WeeklyNewsletters']['product2_banner']);
+            $this->request->data['WeeklyNewsletter']['product2_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['product2_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['product2_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['product2_banner']);
              
-             $this->request->data['WeeklyNewsletters']['brand1_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletters']['brand1_banner']['name'];
-            copy($this->request->data['WeeklyNewsletters']['brand1_banner']['tmp_name'], $this->request->data['WeeklyNewsletters']['brand1_banner']);
+             $this->request->data['WeeklyNewsletter']['brand1_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['brand1_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['brand1_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['brand1_banner']);
 
-            $this->request->data['WeeklyNewsletters']['brand2_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletters']['brand2_banner']['name'];
-            copy($this->request->data['WeeklyNewsletters']['brand2_banner']['tmp_name'], $this->request->data['WeeklyNewsletters']['brand2_banner']);
+            $this->request->data['WeeklyNewsletter']['brand2_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['brand2_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['brand2_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['brand2_banner']);
+
+            $this->request->data['WeeklyNewsletter']['featured_banner'] = 'files/news/'.$this->request->data['WeeklyNewsletter']['featured_file']['name'];
+            copy($this->request->data['WeeklyNewsletter']['featured_file']['tmp_name'], $this->request->data['WeeklyNewsletter']['featured_banner']);
 
 
 
-            if ($this->Vendor->save($this->request->data)) {
-                $this->Session->setFlash(__('The Newsletters has been saved'));
+            if ($this->WeeklyNewsletter->save($this->request->data)) {
+                $this->Session->setFlash(__('The Newsletter has been saved'));
                 $this->redirect(array('action' => 'index'));
             } else {
                 $this->Session->setFlash(__('The Newsletters could not be saved. Please, try again.'));
--- app/View/WeeklyNewsletters/add.ctp
+++ app/View/WeeklyNewsletters/add.ctp
@@ -1,38 +1,39 @@
 <div class="Newsletters form">
-<?php echo $this->Form->create('WeeklyNewsletters', array('type' => 'file')); ?>
+<?php echo $this->Form->create('WeeklyNewsletter', array('type' => 'file')); ?>
 	<fieldset>
 		<legend><?php echo __('Add Newsletters'); ?></legend>
 	<?php
 		echo $this->Form->input('name');
-		echo $this->Form->input('header_banner', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
-		echo $this->Form->input('strip_banner', array('label' => 'Wide Image (200x64px)', 'type' => 'file'));
-		echo $this->Form->input('product1_banner', array('label' => 'Facebook Share Image (200x200px)', 'type' => 'file'));
-		echo $this->Form->input('product2_banner', array('label' => 'Carousel(199x102px)', 'type' => 'file'));
-		echo $this->Form->input('brand1_banner', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
-		echo $this->Form->input('brand2_banner', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
+		echo $this->Form->input('header_file', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
+		echo $this->Form->input('strip_file', array('label' => 'Wide Image (200x64px)', 'type' => 'file'));
+		echo $this->Form->input('product1_file', array('label' => 'Facebook Share Image (200x200px)', 'type' => 'file'));
+		echo $this->Form->input('product2_file', array('label' => 'Carousel(199x102px)', 'type' => 'file'));
+		echo $this->Form->input('brand1_file', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
+		echo $this->Form->input('brand2_file', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
+		echo $this->Form->input('featured_file', array('label' => 'Thumb (50x50px)', 'type' => 'file'));
 
-		echo $this->Tinymce->input('WeeklyNewsletters.brand1_text', array( 
+		echo $this->Tinymce->input('WeeklyNewsletter.brand1_text', array( 
 	            'label' => 'About' 
 		            ),array( 
 		                'language'=>'en' 
 		            ), 
 		            'full' 
 	        ); 
-		echo $this->Tinymce->input('WeeklyNewsletters.brand2_text', array( 
+		echo $this->Tinymce->input('WeeklyNewsletter.brand2_text', array( 
 	            'label' => 'short About' 
 		            ),array( 
 		                'language'=>'en' 
 		            ), 
 		            'full' 
 	        ); 
-		echo $this->Tinymce->input('WeeklyNewsletters.template_text', array( 
+		echo $this->Tinymce->input('WeeklyNewsletter.template_text', array( 
 	            'label' => 'About' 
 		            ),array( 
 		                'language'=>'en' 
 		            ), 
 		            'full' 
 	        ); 
-		echo $this->Tinymce->input('WeeklyNewsletters.template_heading', array( 
+		echo $this->Tinymce->input('WeeklyNewsletter.template_heading', array( 
 	            'label' => 'About' 
 		            ),array( 
 		                'language'=>'en' 
@@ -49,7 +50,7 @@
 	<h3><?php echo __('Actions'); ?></h3>
 	<ul>
 
-		<li><?php echo $this->Html->link(__('List WeeklyNewsletters'), array('action' => 'index')); ?></li>
-		<li><?php echo $this->Html->link(__('New WeeklyNewsletters'), array('controller' => 'WeeklyNewsletters', 'action' => 'add')); ?> </li>
+		<li><?php echo $this->Html->link(__('List WeeklyNewsletter'), array('action' => 'index')); ?></li>
+		<li><?php echo $this->Html->link(__('New WeeklyNewsletter'), array('controller' => 'WeeklyNewsletter', 'action' => 'add')); ?> </li>
 	</ul>
 </div>
