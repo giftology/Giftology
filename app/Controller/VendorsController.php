4c0c507529b0a88c49f6753f6118cdf20d09e04d
prabhatshishodia <prabhat@giftology.com>
2013-05-28 18:22:59 +0530

    Vendor controller search and download
    vendor controller search and download

--- app/Controller/VendorsController.php
+++ app/Controller/VendorsController.php
@@ -7,12 +7,103 @@ App::uses('AppController', 'Controller');
  */
 class VendorsController extends AppController {
 	public $helpers = array('Minify.Minify');
+
+public $components = array('Search.Prg'); 
 /**
  * index method
  *
  * @return void
  */
+public $presetVars = array(
+            array('field' => 'id', 'type' => 'value'),
+            array('field' => 'name', 'type' => 'value'),
+            
+            array('field'=> 'created','type'=>'value'),
+            array('field'=> 'modified','type'=>'value'),
+        
+        );
+         public function download_vendor_csv(){
+				
+                    $filename = "Vendors ".date("Y.m.d").".csv";
+                    $csv_file = fopen('php://output', 'w');
+                    header('Content-type: application/csv');
+                    header('Content-Disposition: attachment; filename="'.$filename.'"');
+                    $header_row= array('Id','Vendor Name','Created','Modified');
+                    fputcsv($csv_file,$header_row,',','"');
+                    if( !empty( $this->data ))
+                    {
+                        foreach($this->data['chk1'] as $id)  
+                        {
+                            $ab=" ";
+                            $result= $this->Vendor->find('first', array('conditions'=>array('Vendor.id'=>$id)));
+                            $row = array(
+                            $result['Vendor']['id'],
+                            $result['Vendor']['name'],
+                            $result['Vendor']['created'],
+                            $result['Vendor']['modified'],
+
+                             );
+                            fputcsv($csv_file,$row,',','"');
+                        }
+                    }
+                    die;
+                  	}
+
 	public function index() {
+		 $this->Prg->commonProcess('Vendor');
+	
+		 if(($this->passedArgs['created_start'])||($this->passedArgs['modified_start']))
+        { 
+            if(!($this->passedArgs['created_start'])){
+                 $modified_end=$this->passedArgs['modified_end'].' 23:59:59';
+                 $modified_start=$this->passedArgs['modified_start'].' 00:00:00';
+                if(!$this->passedArgs['modified_end']){
+                    $modified_end=$this->passedArgs['modified_start'].' 23:59:59';
+                }
+                
+               $conditions=array('conditions' => array($this->Vendor->parseCriteria($this->passedArgs),'Vendor.modified >'=>$modified_start,'Vendor.modified <' => $modified_end
+               
+               ),'order'=>array('Vendor.modified'=>'DESC')); 
+            }
+            
+            if(!($this->passedArgs['modified_start'])){
+                 $created_end=$this->passedArgs['created_end'].' 23:59:59';
+                 $created_start=$this->passedArgs['created_start'].' 00:00:00';
+                if(!$this->passedArgs['created_end']){
+                    $created_end=$this->passedArgs['created_start'].' 23:59:59';
+                }
+             $conditions=array('conditions' => array($this->Vendor->parseCriteria($this->passedArgs) ,'Vendor.created >'=>$created_start,'Vendor.created <' => $created_end
+               ),'order'=>array('Vendor.modified'=>'DESC')); 
+            }
+
+
+        
+           
+           if(($this->passedArgs['created_start'])&&(($this->passedArgs['modified_start'])) )
+            { 
+                 $modified_end=$this->passedArgs['modified_end'].' 23:59:59';
+                 $modified_start=$this->passedArgs['modified_start'].' 00:00:00';
+                 $created_end=$this->passedArgs['created_end'].' 23:59:59';
+                 $created_start=$this->passedArgs['created_start'].' 00:00:00';
+                if(!$this->passedArgs['modified_end']){
+                    $modified_end=$this->passedArgs['modified_start'].' 23:59:59';
+                }
+                if(!$this->passedArgs['created_end']){
+                    $created_end=$this->passedArgs['created_start'].' 23:59:59';
+                }
+                
+          $conditions=array('conditions' => array($this->Vendor->parseCriteria($this->passedArgs),'Vendor.modified >'=>$modified_start,'Vendor.modified <' => $modified_end
+           ,'Vendor.created >'=>$created_start,'Vendor.created <' => $created_end
+            ),'order'=>array('Vendor.modified'=>'DESC'));  
+             }  
+            
+    
+        }
+        else{
+		$conditions= array('conditions' => array($this->Vendor->parseCriteria($this->passedArgs)
+   ),'order'=>array('Vendor.modified'=>'DESC'));
+}
+		$this->paginate = $conditions;
 		$this->Vendor->recursive = 0;
 		$this->set('vendors', $this->paginate());
 	}
@@ -40,7 +131,41 @@ class VendorsController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Vendor->create();
+			$this->Vendor->set($this->request->data);
+			if (!$this->Vendor->validates())
+            {
+                 $errors1 = $this->Vendor->validationErrors;
+                 $errors=(array_values($errors1));
+                 $errorString1 = null;
+                   foreach($errors as $err)
+                   {
+                       if($errorString1 == null)
+                           $errorString1 = $err[0];
+                       else
+                           $errorString1 = $errorString1.', '.$err[0];
+                       
+                   }
+                $finalErrorString = 'Please Enter: '.$errorString1.'<br/>';
+                $this->Session->setFlash(__($finalErrorString));
+                $this->redirect(array('controller' => 'vendors', 'action'=>'add'));   
+            }
 			
+			if($this->data['Vendor']['description'] == "")
+			{
+				$this->Session->setFlash(__('Please enter the description'));
+                    $this->redirect(array('controller' => 'vendors', 'action'=>'add'));   
+			}
+			$error_array= array();
+                $allowed =  array('png' ,'jpg');
+                $i = 1;
+                foreach($_FILES['data']['name']['Vendor'] as $file){
+                        $ext = pathinfo($file, PATHINFO_EXTENSION);
+                        if ($i++ == 4) break;
+                        if(!in_array($ext,$allowed) ) {
+                            $error_array[]=  $file;
+                        }      
+                    }
+            if(!$error_array){
 			//facebook linter doesnt like image links with space in them, convert all space to underscore	
 			$this->request->data['Vendor']['thumb_file']['name']
 				= $this->request->data['Vendor']['name'].str_replace(" ","_", $this->request->data['Vendor']['thumb_file']['name']);
@@ -62,7 +187,19 @@ class VendorsController extends AppController {
 
 			$this->request->data['Vendor']['carousel_image'] = 'files/carousel/'.$this->request->data['Vendor']['carousel_image_file']['name'];
 			copy($this->request->data['Vendor']['carousel_image_file']['tmp_name'], $this->request->data['Vendor']['carousel_image']);
-
+		}
+		else { 
+            	$err1;
+                    foreach($error_array as $err){
+                        $err1= $err1.' ';
+                        $err1= $err1.$err.' ' ;
+                        
+                        
+                        
+                    }
+                    $this->Session->setFlash(__('Please upload an image in JPEG/PNG format only'));
+                    $this->redirect(array('controller' => 'vendors', 'action'=>'add'));   
+                }
 
 			if ($this->Vendor->save($this->request->data)) {
 				$this->Session->setFlash(__('The vendor has been saved'));
--- app/View/Vendors/index.ctp
+++ app/View/Vendors/index.ctp
@@ -1,7 +1,44 @@
+<?= $this->element('admin_header'); ?>
 <div class="vendors index">
+
 	<h2><?php echo __('Vendors'); ?></h2>
-	<table cellpadding="0" cellspacing="0">
-	<tr>
+
+	
+
+		<div id="collapse1"  class="backSearch" style="border:1px solid #ccc; width:800px; padding:10px; margin-bottom:70px;">
+		<?php echo $this->Form->create('Vendor', array('url' => array_merge(array('action' => 'index'), $this->params['pass']))); 
+?>  
+        <td><?php echo $this->Form->input('id', array('type'=>'text','div' => false,'label'=>'','size'=>'1','placeholder'=>'Id'));?></td>
+        <td><?php echo $this->Form->input('name', array('type'=>'text','div' => false,'label'=>'','size'=>'10','placeholder'=>' Name'));?></td>
+        
+      
+        <td><?php echo $this->Form->input('created_start', array('type'=>'text','div' => false,'label'=>'','size'=>'15','id'=>'datepicker2',"placeholder"=>'Created Start Date'));?>
+		<?php echo $this->Form->input('created_end', array('type'=>'text','div' => false,'label'=>'','size'=>'15','id'=>'datepicker3','placeholder'=>'Created End Date'));?>
+        </td>
+        <td><?php echo $this->Form->input('modified_start', array('type'=>'text','div' => false,'label'=>'','size'=>'15','id'=>'datepicker4',"placeholder"=>'Modified Start Date'));?>
+			<?php echo $this->Form->input('modified_end', array('type'=>'text','div' => false,'label'=>'','size'=>'15','id'=>'datepicker5','placeholder'=>'Modified End Date'));?>
+        </td>
+        
+       <td>
+         <?php echo $this->Form->submit(__('Search', true), array('div' => false));	
+        if (isset($this->params['named']) & !empty($this->params['named'])){ 
+            echo $this->Html->link(_('Reset Filter'), array('controller'=>'Vendors','action'=>'index'));
+        } 
+        ?>
+        </td>			
+    </tr>
+                 <?php echo $this->Form->end();?></td>
+                 </div>
+	 <?php echo $this->Form->create( '', array( 'id'=>'frm1' ,'name'=>'frm1' ,'controller'=>'Vendors', 'action' => 'download_vendor_csv', 'onsubmit'=>'return chkValidate();') );?>
+        <table class="grd-chkbox" cellpadding="0" cellspacing="0" id="ordrMgmt">
+        	<div class="download_csv">
+             <?php  echo $this->Form->submit("Download Vendor CSV" ,array( 'name'=>'csv', 'class'=>'button','type'=>'submit', 'id'=>'assign' , 'label' =>'','value'=>"" ));	
+              echo $this->Form->end();
+             ?>
+             </div>
+	<table cellpadding="0" cellspacing="0" border="1">
+	<tr><td class="campaign_checkbo"> <input class="campaign_checkbox" type="checkbox" name="checkall"onclick='checkedAll(frm1);' > </td>
+
 			<th><?php echo $this->Paginator->sort('id'); ?></th>
 			<th><?php echo $this->Paginator->sort('name'); ?></th>
 			<th><?php echo $this->Paginator->sort('created'); ?></th>
@@ -11,6 +48,7 @@
 	<?php
 	foreach ($vendors as $vendor): ?>
 	<tr>
+		<td class="campaign_checkbo"><input class="campaign_checkbox" type="checkbox" name="chk1[]" id="<?php echo $vendor['Vendor']['id'];?>" value="<?php echo $vendor['Vendor']['id']?>"> </td>
 		<td><?php echo h($vendor['Vendor']['id']); ?>&nbsp;</td>
 		<td><?php echo h($vendor['Vendor']['name']); ?>&nbsp;</td>
 		<td><?php echo h($vendor['Vendor']['created']); ?>&nbsp;</td>
@@ -23,20 +61,27 @@
 	</tr>
 <?php endforeach; ?>
 	</table>
-	<p>
+	  <div class="paging">
 	<?php
-	echo $this->Paginator->counter(array(
-	'format' => __('Page {:page} of {:pages}, showing {:current} records out of {:count} total, starting on record {:start}, ending on {:end}')
-	));
-	?>	</p>
-
-	<div class="paging">
-	<?php
-		echo $this->Paginator->prev('< ' . __('previous'), array(), null, array('class' => 'prev disabled'));
-		echo $this->Paginator->numbers(array('separator' => ''));
-		echo $this->Paginator->next(__('next') . ' >', array(), null, array('class' => 'next disabled'));
+	    if ($this->Paginator->hasPrev()) {
+		echo $this->Paginator->prev($this->html->image('35x35_prev.png' , array("title" => "Prev")), array('escape' => false), null, array('class' => 'prev disabled'));
+	}
+		 if ($this->Paginator->hasNext()) {
+		 echo $this->Paginator->next($this->html->image('35x35_next.png' , array("title" => "next")),array('escape' => false), null, array('class' => 'next disabled'));
+	}
 	?>
-	</div>
+    </div>
+    <div style="width:auto;height:auto;margin-top:-18px;margin-left:100px;" >
+		<br><?php echo $this->Paginator->numbers(array('separator' => ' | ','modulus'=>'10','first'=>'First Page ','last'=>' Last Page'));?>
+    </div> 
+    <div style="width:auto;height:auto;margin-top:10px;margin-left:50px;" >
+			<?php
+			echo $this->Paginator->counter(array(
+				'format' => __('Page {:page} of {:pages}, showing {:current} records out of {:count} total, starting on record {:start}, ending on {:end}')
+				));
+				?>	
+    </div>
+     <br>
 </div>
 <div class="actions">
 	<h3><?php echo __('Actions'); ?></h3>
@@ -46,3 +91,89 @@
 		<li><?php echo $this->Html->link(__('New Product'), array('controller' => 'products', 'action' => 'add')); ?> </li>
 	</ul>
 </div>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+ 
+  <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+  <script>
+  $(function() {
+  	
+    $( "#datepicker" ).datepicker({
+     dateFormat: 'yy-mm-dd',
+      buttonText: "Choose the date",
+  });
+     $( "#datepicker1" ).datepicker({
+     dateFormat: 'yy-mm-dd',
+      buttonText: "Choose the date",
+  });
+      $( "#datepicker2" ).datepicker({
+     dateFormat: 'yy-mm-dd',
+      buttonText: "Choose the date",
+  });
+         $( "#datepicker3" ).datepicker({
+     dateFormat: 'yy-mm-dd',
+      buttonText: "Choose the date",
+  });
+            $( "#datepicker4" ).datepicker({
+     dateFormat: 'yy-mm-dd',
+      buttonText: "Choose the date",
+  });
+               $( "#datepicker5" ).datepicker({
+     dateFormat: 'yy-mm-dd',
+      buttonText: "Choose the date",
+  });
+});
+  </script>
+
+<script>
+		$(document).ready(function() {
+			$('#collapse1').hide();
+			
+		  $('.nav-toggle').click(function(){
+
+			//get collapse content selector
+			var collapse_content_selector = $(this).attr('href');					
+ 
+			//make the collapse content to be shown or hide
+			var toggle_switch = $(this);
+			$(collapse_content_selector).toggle(function(){
+			  if($(this).css('display')=='none'){
+                                //change the button label to be 'Show'
+				toggle_switch.html('Search(+)');
+			  }else{
+                                //change the button label to be 'Hide'
+				toggle_switch.html('Search(-)');
+			  }
+			});
+		  });
+ 
+		});	
+		</script>
+<script>
+    function chkValidate(){
+        if($("[name='chk1[]']:checked").length<1){
+            alert('Please select atleast one record.');
+            return false;
+        }
+        return true;
+    }
+    checked=false;
+ function checkedAll (frm1) {
+ 
+	var aa= document.getElementById('frm1');
+	 if (checked == false)
+        {
+           checked = true
+          }
+        else        {
+          checked = false
+          }
+	for (var i =0; i < aa.elements.length; i++) 
+	{
+		 aa.elements[i].checked = checked;
+	}
+	
+ }
+ $(document).ready(function(){
+        $('.campaign_checkbox').show();
+    });
+</script>
