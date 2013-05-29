ca5366bd28e80ea625a95203aaf3e4f120da070e
swapnil <nswapt@gmail.com>
2013-05-29 11:40:06 +0530

    done

--- app/Controller/WeeklyNewslettersController.php
+++ app/Controller/WeeklyNewslettersController.php
@@ -201,30 +201,5 @@ class WeeklyNewslettersController extends AppController {
          }
      }
 //////////////////////mail function////////////////////////
-    public function fridayNewsletter(){
-
-
-
-      /*  $user_id = isset($this->params->query['user_id']) ? $this->params->query['user_id'] : null;
-        $conditions = array(
-            'user_id' => $user_id);
-
-        $conditions['MONTH(friend_birthday)'] = date('m');
-        $conditions['DAY(friend_birthday)'] = date('d'); 
-        $e = $this->wsReminderTodayException($user_id, $conditions);
-        if(isset($e) && !empty($e)) $this->set('today_birthday', array('error' => $e));
-        else{
-            $this->log("Searching today birthday reminder for ".$user_id);
-            $reminders = $this->Reminder->find('all',
-                array('conditions' => $conditions,
-                    'order' => 'friend_birthday ASC'
-            ));
-            $this->set('today_birthday', $reminders);    
-        }
-        $this->set('_serialize', array('today_birthday')); */
-    }
-
-
-   
 }
 ?>
