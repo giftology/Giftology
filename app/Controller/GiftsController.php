97cd86e4e922eccf8d11db34a65d83dc921447de
Shubham <shubham150@gmail.com>
2013-06-06 16:00:56 +0530

    temporary_coupon_code

--- app/Controller/GiftsController.php
+++ app/Controller/GiftsController.php
@@ -1269,7 +1269,7 @@ public function index() {
                 );
 
         }
-        $gift_claimable=$this->Gift->find('first',array('fields'=>array('id'),'conditions' => array('Gift.receiver_id' => $this->Auth->user('id'),'Gift.claim' => 0,'Gift.redeem' => 0,'Gift.expiry_date >' => date('Y-m-d'),'Gift.gift_status_id' => 1)));
+        $gift_claimable=$this->Gift->find('first',array('order'=>'Gift.id DESC','fields'=>array('id'),'conditions' => array('Gift.receiver_id' => $this->Auth->user('id'),'Gift.claim' => 0,'Gift.redeem' => 0,'Gift.expiry_date >' => date('Y-m-d'),'Gift.gift_status_id' => 1)));
         $this->set('us',$gift_claimable['Gift']['id']);
          $gift = $this->Gift->find('first', array(
             'contain' => array(
