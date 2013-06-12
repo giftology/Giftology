7448c7271c13ed74284d88f247c7864777749b00
Swapnil Tripathi <nswapt@gmail.com>
2013-06-12 12:57:12 +0530

    countdown modification

--- app/webroot/js/scrit1.js
+++ app/webroot/js/scrit1.js
@@ -19,28 +19,28 @@ $(function(){
 			
 			var message = " Your voucher expires in ";
 			
-			if(minutes==0&& seconds>=10){
-			message +="<b>"+ minutes + "0:" + ( minutes==1 ? '':'' ) ;
+			if(minutes==0 && seconds<10){
+			message +="<b>"+ minutes + "0:0" + ( minutes==1 ? '':'' ) ;
 			message += seconds+"</b>"  + ( seconds==1 ? '':'' ) + "  <br />";
+			if(minutes==0 && seconds<1){
+				window.location.replace("http://google.com");
+			}
 			note.html(message);
 			
 			}
 			
-			
-			
 			else{
-			message +="<b>"+ minutes + " 0:0" + ( minutes==1 ? '':'' ) ;
+			message +="<b>"+ minutes + ":" + ( minutes==1 ? '':'' ) ;
 			message += seconds+"</b>"  + ( seconds==1 ? '':'' ) + "  <br />";
 			
-			if(minutes==0 && seconds<1){
-				window.location.replace("http://www.giftology.com");
-			}
+			
 			
 				note.html(message);
 			}
 				
 		}
 	});
+		
 });
 	
 });
