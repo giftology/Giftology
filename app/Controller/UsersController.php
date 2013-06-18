29f616922331e68e0e510cd2d4986962e01b561b
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-06-18 18:48:58 +0530

    removing update_defaulters code

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1212,21 +1212,6 @@ public function download_user_csv_all($download_selected = null){
         return $defaulter;
     }
 
-    public function update_defaulters(){
-        $defaulters = array(1851691593,100005543981473,100000678486376,100004488369286,10000396701270,582264226,1792236841,100001972394330,100002449145797,1566298187,100001279783328,1234441696,100001279783328,100000377682936,677152056,100001418953825,100005543981473,100005501501914,100001392543171,1672249409,1566298187,1139626999,100005501501914,100004734788325,100004094964735,100000466429903,100000730434494,100000466429903,100000730434494);
-        $white_listed_users = array(100004342150637);
-        /*$new_defaulters_list = array_diff($defaulters, $white_listed_users);
-        $defaulter = in_array($user_fb_id, $new_defaulters_list);*/
-
-        foreach($defaulters as $defaulter){
-            set_time_limit(60);
-            $this->User->updateAll(array('defaulter' => 1), array('facebook_id' => $defaulter));
-        }
-
-        foreach($white_listed_users as $defaulter){
-            $this->User->updateAll(array('defaulter' => 0), array('facebook_id' => $defaulter));
-        }
-    }
     /*public function wsAddRefreshReminder($json_data){
         
     }*/
