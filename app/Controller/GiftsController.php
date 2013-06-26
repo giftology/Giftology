9717edb734ad8596a76eaf8a8688dfdff4c02952
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-06-26 17:46:26 +0530

    mobile app testing

--- app/Controller/GiftsController.php
+++ app/Controller/GiftsController.php
@@ -111,7 +111,7 @@ class GiftsController extends AppController {
         $gift_id = isset($this->params->query['gift_id']) ? $this->params->query['gift_id'] : null;
         $receiver_fb_id = isset($this->params->query['receiver_fb_id']) ? $this->params->query['receiver_fb_id'] : null;
         $e = $this->wsRdeemGiftException($gift_id, $receiver_fb_id);
-        $code = $this->Gift->find('count',array('field' => array('temporary_code'), 'conditions' => array('id' => $gift_id, 'temporary_code IS NOT NULL')));
+        $code = $this->Gift->find('count',array('conditions' => array('id' => $gift_id, 'temporary_code IS NOT NULL')));
         if(isset($e) && !empty($e)) $this->set('gift', array('error' => $e));
         else{
             if($code){
