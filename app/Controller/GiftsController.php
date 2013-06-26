dc94573bd907964531490a5f922f8a30e466621c
Alok Kumar <alokkumar.iiitm@gmail.com>
2013-06-26 20:52:41 +0530

    blocking certain features products

--- app/Controller/GiftsController.php
+++ app/Controller/GiftsController.php
@@ -711,7 +711,6 @@ public function index() {
 				$this->redirect(array('controller' => 'reminders',
          			'action'=>'view_friends'));
             }
-            DebugBreak();
             $receiver_fb_id=$this->data['gifts']['user_id'];
             $receiver = $this->Connect->User->findByFacebookId($receiver_fb_id);
 
